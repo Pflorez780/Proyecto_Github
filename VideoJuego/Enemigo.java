5877bc156221c6de221ed50328c35f5b83fe351a
Pedro Florez <pflorez@uniempresarial.edu.co>
2026-09-03 21:48:41 -0500

    Creacion metodos constructores superclase

--- VideoJuego/Enemigo.java
+++ VideoJuego/Enemigo.java
@@ -1,7 +1,44 @@
 public class Enemigo {
-    String nombre;
-    int vida;
-    double velocidad;
-    boolean aereo;
+    private String nombre;
+    private int vida;
+    private double velocidad;
+    private boolean aereo;
 
+    public Enemigo(String nombre, int vida, double velocidad, boolean aereo) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.velocidad = velocidad;
+        this.aereo = aereo;
+    }
+
+    public Enemigo(String nombre, int vida) {
+        this.nombre = nombre;
+        this.vida = vida;
+        velocidad = 100.2;
+        aereo =true;
+    }
+
+    public Enemigo(double velocidad, boolean aereo) {
+        this.velocidad = velocidad;
+        this.aereo = aereo;
+    }
+
+    
+    public String getnombre() {
+        return nombre;
+    }
+    public String getvida() {
+        return vida;
+    }
+    public String getvelocidad() {
+        return velocidad;
+    }
+    public String aereo() {
+        return aereo;
+    }
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Vida: " + vida + ", Velocidad: " + velocidad + ", Es Aereo: " + aereo;
+    }
+    
 }
