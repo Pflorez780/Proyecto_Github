de4690be56ded47f571be0ce3110372f041407af
Pedro Florez <pflorez@uniempresarial.edu.co>
2026-09-05 02:25:03 -0500

    Objetos superclase y subclase - CercanoE

--- VideoJuego/Main.java
+++ VideoJuego/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Enemigo enemigo1 = new Enemigo("Lobos", 100, 15.3, false);
+        Enemigo enemigo2 = new Enemigo("Polilla", 20);
+        enemigo2.Setvelocidad(95);
+        enemigo2.Setaereo(true);
+        CercanoE bandidos = new CercanoE("Pueblo humano", 10, 50.5, true);
+        bandidos.Setnombre("Bandidos");
+        bandidos.Setvida(75);
+        bandidos.Setvelocidad(25);
+        bandidos.Setaereo(false);
+        CercanoE cazadores = new CercanoE(100, true);
+        cazadores.Setnombre("Cazadores");
+        cazadores.Setvida(100);
+        cazadores.Setvelocidad(30);
+        cazadores.Setaereo(false);
     }
 }
